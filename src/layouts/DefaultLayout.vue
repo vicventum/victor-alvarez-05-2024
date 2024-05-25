6c7165a590863ee35d687bcaa9fbed699794cfdf
Víctor Álvarez <vicventum.job@gmail.com>
2024-05-24 21:59:32 -0400

    Configura estilos base

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <div>
+    <header>
+			Header
+		
+			<nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+		</header>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
