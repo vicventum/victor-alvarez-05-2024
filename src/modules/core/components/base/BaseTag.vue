d4c925fd31188d9a5e3ab879f8e32924f97ec379
Víctor Álvarez <vicventum.job@gmail.com>
2024-05-26 23:38:11 -0400

    Deploy a GitHub Pages

--- src/modules/core/components/base/BaseTag.vue
+++ src/modules/core/components/base/BaseTag.vue
@@ -8,9 +8,13 @@
 
 <style lang="scss" scoped>
 .tag {
-  padding: 0.1rem 0.5rem 0.4rem;
+  padding: 0.1rem 0.5rem 0.2rem;
   color: var(--light);
   background-color: var(--secondary);
   border-radius: 1rem;
+
+  @media (min-width: 400px) {
+    padding-bottom: 0.3rem;
+  }
 }
 </style>
